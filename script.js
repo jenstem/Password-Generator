d5ef85400000aa930b220b3a7e8e8a5989211fd4
jenstem <124745888+jenstem@users.noreply.github.com>
2023-06-19 08:29:12 -0400

    Add uppercase var

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");

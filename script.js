eec6a103e5f49621ce31effcd47839da39d67598
jenstem <124745888+jenstem@users.noreply.github.com>
2023-06-19 08:28:44 -0400

    Add lowercase var

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 

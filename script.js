906697514014014a834ab44d35ec88d0da4064c4
jenstem <124745888+jenstem@users.noreply.github.com>
2023-06-19 08:40:55 -0400

    Pseudo for loop

--- script.js
+++ script.js
@@ -26,5 +26,7 @@ function generatePassword() {
 
 // will need an IF statement
 
+// will need a FOR loop
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
